8cc7799767ee2ee9a47d500d7dcb79d53eac8edb
Z_J_WANG <z.j.wang@oracle.com>
2021-05-11 16:42:50 +0800

    test elasticsearch

--- src/main/java/wzjtech/test/spring/entity/SchoolDocument.java
+++ src/main/java/wzjtech/test/spring/entity/SchoolDocument.java
@@ -1,8 +1,6 @@
 package wzjtech.test.spring.entity;
 
 import java.io.Serializable;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.elasticsearch.annotations.Document;
@@ -12,13 +10,37 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 // index can be treated as database
 @Document(indexName = "student")
 @TypeAlias("school")
-@Getter
-@Setter
-public class SchoolDocument implements Serializable {
+// @Getter
+// @Setter
+public class SchoolDocument {
   @Id private String id;
 
   @Field(type = FieldType.Text)
   private String name;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getIntroduction() {
+    return introduction;
+  }
+
+  public void setIntroduction(String introduction) {
+    this.introduction = introduction;
+  }
 
   @Field(type = FieldType.Text)
   private String introduction;
